96a698658bca818e167fcad5f2ba860fe65ccc0e
Berkay Sazak <berkaysazak313@gmail.com>
2022-12-27 19:43:01 +0300

    Commit

--- app/src/main/java/com/example/remotejobsapp/MainActivity.kt
+++ app/src/main/java/com/example/remotejobsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.remotejobsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.remotejobsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = ""
     }
 }
---
+++ app/src/main/java/com/example/remotejobsapp/view/SearchJobFragment.kt
@@ -0,0 +1,25 @@
+package com.example.remotejobsapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.remotejobsapp.R
+
+class SearchJobFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_job, container, false)
+    }
+
+}
